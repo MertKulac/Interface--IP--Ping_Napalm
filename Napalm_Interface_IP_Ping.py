545847ab6d367f3724e557f7b9bbb344e156dee1
Mert Kulac <96883175+MertKulac@users.noreply.github.com>
2022-04-12 14:41:56 +0300

    Update Napalm_Interface_IP_Ping.py

--- Napalm_Interface_IP_Ping.py
+++ Napalm_Interface_IP_Ping.py
@@ -1,21 +1,23 @@
 from napalm import get_network_driver
 from ping3 import ping
 from ipaddress import ip_network, ip_address
+import pprint
 
 driver = get_network_driver('huawei_vrp')
-device = driver(hostname='172.28.111.86', username='NPESCRIPT', password='RYXga421')
+device = driver(hostname='X.X.X.X', username='admin', password='admin')
 device.open()
-
 
 #  Return general device information
 get_interfaces_ip = device.get_interfaces_ip()
+print(get_interfaces_ip)
 
 for i in get_interfaces_ip:
         if "Virtual-Ether" in i:
                 interface = get_interfaces_ip[i]["ipv4"]
                 ip_list = list(interface.keys())
-                #print(ip_list)
+                print(ip_list)
+
                 for j in ip_list:
-                        send_command = device.cli(['ping {}'.format(j)])
-                        print(send_command)
+                        ping_remote_ip = device.ping(j)
+                        pprint.pprint(ping_remote_ip)
                     
